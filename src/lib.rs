881d778e8dfd82924de7032d140a8aa83c846032
Guilherme Lima <git@glima31.dev>
2026-02-06 22:10:28 +0000

    feat: define SyroError

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,14 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+use syro_sys::*;
+use thiserror::Error;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+#[derive(Clone, Debug, Error)]
+pub enum SyroError {
+    #[error("SyroVolcaSample_Start failed with status {0}")]
+    VolcaSampleStart(SyroStatus),
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+    #[error("SyroVolcaSample_GetSample failed with status {0}")]
+    VolcaSampleGetSample(SyroStatus),
+
+    #[error("SyroVolcaSample_End failed with status {0}")]
+    VolcaSampleEnd(SyroStatus),
 }
